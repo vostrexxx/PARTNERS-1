4469b8860fce239505159ade9eb42f5f15f8d402
Oleg <olegmuk@mail.ru>
2023-07-03 21:56:12 +0300

    Created Spring framework environment

---
+++ src/main/java/com/application/PARTNERS/PartnersApplication.java
@@ -0,0 +1,13 @@
+package com.application.PARTNERS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/PARTNERS/PartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.PARTNERS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
